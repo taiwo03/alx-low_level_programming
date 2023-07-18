fe08d26d08472733510791dac9f0180edd2a8d00
taiwo03 <taiwoogunks@gmail.com>
2023-07-18 12:58:35 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*main - program that prints text
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
