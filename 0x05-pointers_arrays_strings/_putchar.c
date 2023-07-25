9c422fcac8c0ae99131204b7d02ecd7c3a8ff4ff
taiwo03 <taiwoogunks@gmail.com>
2023-07-25 11:21:31 +0300

    I do not fear computers

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -9,5 +10,5 @@
  */
 int _putchar(char c)
 {
-	return (1 &c 1);
+	return (write(1, &c, 1));
 }
