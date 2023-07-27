8e600fc7cbeaf331921657052215bf921984a0e6
taiwo03 <taiwoogunks@gmail.com>
2023-07-27 12:26:37 +0300

    strncpy

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -2,8 +2,10 @@
 
 /**
  * _putchar - checks the code
+ * @c; The character to print
  *
- * Return: 0
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
  */
 int _putchar(char c)
 {
