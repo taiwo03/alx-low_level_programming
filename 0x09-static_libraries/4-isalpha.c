8e06ffa14f228340a10844325b55d11f9a0c4577
taiwo03 <taiwoogunks@gmail.com>
2023-08-04 12:29:52 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic order
+ *
+ * @c: alphabet to be verified
+ *
+ * Return:0 or 1
+ */
+
+int _isalpha(int c)
+{
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
